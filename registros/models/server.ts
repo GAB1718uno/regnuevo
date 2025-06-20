0e093bf390490bf3b74de5a43bd8c1174bde0fac
Gilson Albino Barbosa <gilsonbbs@hotmail.com>
2025-06-20 22:44:08 +0200

    ssl desactivado 2

--- registros/models/server.ts
+++ registros/models/server.ts
@@ -51,6 +51,8 @@ class Server {
     const allowedOrigins = [
       "http://localhost:3000",
       "http://localhost:4000",
+      "https://cementrio.com.es",
+      "https://167.71.36.17",
       "https://cementereact.vercel.app",
       "https://cementereact-hn8is223k-gilsons-projects-5a42cee5.vercel.app"
     ];
